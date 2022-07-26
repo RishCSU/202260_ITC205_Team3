d4dfe7d9083ec591db2196898c5595b479a8c4f5
Deepak <dvcha0@eq.edu.au>
2022-07-27 07:03:05 +1000

    Revert "Changed variabl names based on rule 2"
    This reverts commit e2417d0ce5205f12392308de19b888de1905d2ad.

--- BRLS/src/library/entities/Item.java
+++ BRLS/src/library/entities/Item.java
@@ -6,47 +6,47 @@ import java.io.Serializable;
 public class Item implements Serializable {
 	
 	private ItemType type;
-
-	private String author;
+	//TyPe
+	private String aUtHoR;
 	// aUtHoR
-	private String title;
-	private String callNo;
-	private long id;
+	private String TiTlE;
+	private String CaLlNo;
+	private long Id;
 	
 
 	private enum iTeM_StAtE { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
-	private iTeM_StAtE state;
+	private iTeM_StAtE sTaTe;
 	
 	
-	public Item(String author, String title, String callNo, ItemType itemType, long id) {
-		this.type = itemType;
-		this.author = author;
-		this.title = title;
-		this.callNo = callNo;
-		this.id = id;
 
-		this.state = iTeM_StAtE.AVAILABLE;
+	public Item(String AuThOr, String tItLe, String cAlLnO, ItemType ItEmTyPe, long iD) {
+		this.type = ItEmTyPe;
+		this.aUtHoR = AuThOr;
+		this.TiTlE = tItLe;
+		this.CaLlNo = cAlLnO;
+		this.Id = iD;
+		this.sTaTe = iTeM_StAtE.AVAILABLE;
 	}
 	
 	public String toString() {
 		StringBuilder Sb = new StringBuilder();
-		Sb.append("Item: ").append(id).append("\n")
+		Sb.append("Item: ").append(Id).append("\n")
 		  .append("  Type:   ").append(type).append("\n")
-		  .append("  Title:  ").append(title).append("\n")
-		  .append("  Author: ").append(author).append("\n")
-		  .append("  CallNo: ").append(callNo).append("\n")
-		  .append("  State:  ").append(state);
+		  .append("  Title:  ").append(TiTlE).append("\n")
+		  .append("  Author: ").append(aUtHoR).append("\n")
+		  .append("  CallNo: ").append(CaLlNo).append("\n")
+		  .append("  State:  ").append(sTaTe);
 		
 		return Sb.toString();
 	}
 
 
 	public Long GeTiD() {
-		return id;
+		return Id;
 	}
 
 	public String GeTtItLe() {
-		return title;
+		return TiTlE;
 	}
 
 	public ItemType getType() {
@@ -55,51 +55,56 @@ public class Item implements Serializable {
 
 
 	public boolean Is_AvAiLaBlE() {
-		return state == iTeM_StAtE.AVAILABLE;
+		return sTaTe == iTeM_StAtE.AVAILABLE;
 	}
 
 	
 	public boolean Is_On_LoAn() {
-		return state == iTeM_StAtE.ON_LOAN;
+		return sTaTe == iTeM_StAtE.ON_LOAN;
 	}
 
 	
 	public boolean Is_DaMaGeD() {
-		return state == iTeM_StAtE.DAMAGED;
+		return sTaTe == iTeM_StAtE.DAMAGED;
 	}
 
 	
 	public void TaKeOuT() {
-		if (state.equals(iTeM_StAtE.AVAILABLE))
-			state = iTeM_StAtE.ON_LOAN;
 
+		if (sTaTe.equals(iTeM_StAtE.AVAILABLE)) 
+			sTaTe = iTeM_StAtE.ON_LOAN;
+		
 		else 
-			throw new RuntimeException(String.format("Item: cannot borrow item while item is in state: %s", state));
-
+			throw new RuntimeException(String.format("Item: cannot borrow item while item is in state: %s", sTaTe));
+		
+		
 	}
 
 
 	public void TaKeBaCk(boolean DaMaGeD) {
-		if (state.equals(iTeM_StAtE.ON_LOAN))
+		if (sTaTe.equals(iTeM_StAtE.ON_LOAN)) 
 			if (DaMaGeD) 
-				state = iTeM_StAtE.DAMAGED;
+				sTaTe = iTeM_StAtE.DAMAGED;			
 
 			else 
-				state = iTeM_StAtE.AVAILABLE;
-		
+
+				sTaTe = iTeM_StAtE.AVAILABLE;		
+
+
 		else 
-			throw new RuntimeException(String.format("Item: cannot return item while item is in state: %s", state));
+			throw new RuntimeException(String.format("Item: cannot return item while item is in state: %s", sTaTe));
 				
 	}
 
 	
 
 	public void rEpAiR() {
-		if (state.equals(iTeM_StAtE.DAMAGED))
-			state = iTeM_StAtE.AVAILABLE;
 
-		else 
-			throw new RuntimeException(String.format("Item: cannot repair while Item is in state: %s", state));
+		if (sTaTe.equals(iTeM_StAtE.DAMAGED)) 
+			sTaTe = iTeM_StAtE.AVAILABLE;
+		
+		else
+			throw new RuntimeException(String.format("Item: cannot repair while Item is in state: %s", sTaTe));
 		
 	}
 
