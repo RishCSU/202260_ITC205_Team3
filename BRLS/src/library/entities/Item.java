3b39d16689a415a84560a9b90118434c0834ea28
Deepak <dchand5986@gmail.com>
2022-07-26 22:30:20 +1000

    Changed method names based on Item class diagram and rule 3

--- BRLS/src/library/entities/Item.java
+++ BRLS/src/library/entities/Item.java
@@ -13,8 +13,9 @@ public class Item implements Serializable {
 	private String callNo;
 	private long id;
 	
-	private enum iTeM_StAtE { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
-	private iTeM_StAtE state;
+	private enum ItemState { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
+	// https://www.w3schools.com/java/java_enums.asp
+	private ItemState state;
 	
 	
 	public Item(String author, String title, String callNo, ItemType itemType, long id) {
@@ -23,7 +24,7 @@ public class Item implements Serializable {
 		this.title = title;
 		this.callNo = callNo;
 		this.id = id;
-		this.state = iTeM_StAtE.AVAILABLE;
+		this.state = ItemState.AVAILABLE;
 	}
 	
 	public String toString() {
@@ -53,23 +54,23 @@ public class Item implements Serializable {
 
 	
 	public boolean isAvailable() {
-		return state == iTeM_StAtE.AVAILABLE;
+		return state == ItemState.AVAILABLE;
 	}
 
 	
 	public boolean isOnLoan() {
-		return state == iTeM_StAtE.ON_LOAN;
+		return state == ItemState.ON_LOAN;
 	}
 
 	
 	public boolean isDamaged() {
-		return state == iTeM_StAtE.DAMAGED;
+		return state == ItemState.DAMAGED;
 	}
 
 	
 	public void takeOut() {
-		if (state.equals(iTeM_StAtE.AVAILABLE))
-			state = iTeM_StAtE.ON_LOAN;
+		if (state.equals(ItemState.AVAILABLE))
+			state = ItemState.ON_LOAN;
 		
 		else 
 			throw new RuntimeException(String.format("Item: cannot borrow item while item is in state: %s", state));
@@ -79,12 +80,12 @@ public class Item implements Serializable {
 
 
 	public void takeBack(boolean damaged) {
-		if (state.equals(iTeM_StAtE.ON_LOAN))
+		if (state.equals(ItemState.ON_LOAN))
 			if (damaged)
-				state = iTeM_StAtE.DAMAGED;
+				state = ItemState.DAMAGED;
 
 			else 
-				state = iTeM_StAtE.AVAILABLE;
+				state = ItemState.AVAILABLE;
 
 		
 		else 
@@ -94,8 +95,8 @@ public class Item implements Serializable {
 
 	
 	public void repair() {
-		if (state.equals(iTeM_StAtE.DAMAGED))
-			state = iTeM_StAtE.AVAILABLE;
+		if (state.equals(ItemState.DAMAGED))
+			state = ItemState.AVAILABLE;
 		
 		else 
 			throw new RuntimeException(String.format("Item: cannot repair while Item is in state: %s", state));
