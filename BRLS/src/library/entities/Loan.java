0c018dde3a64e24cfeef65caf7f318782faeb536
Deepak <dvcha0@eq.edu.au>
2022-07-27 20:52:23 +1000

    Final reviews for Loan.java

--- BRLS/src/library/entities/Loan.java
+++ BRLS/src/library/entities/Loan.java
@@ -5,16 +5,12 @@ import java.util.Date;
 
 @SuppressWarnings("serial")
 public class Loan implements Serializable {
-
 	private enum LoanState { CURRENT, OVER_DUE, DISCHARGED };
-	
 	private long loanId;
 	private Item item;
 	private Patron patron;
 	private Date dueDate;
 	private LoanState state;
-
-	
 	public Loan(long loanId, Item item, Patron patron, Date dueDate) {
 		this.loanId = loanId;
 		this.item = item;
@@ -22,56 +18,41 @@ public class Loan implements Serializable {
 		this.dueDate = dueDate;
 		this.state = LoanState.CURRENT;
 	}
-
 	public void updateStatus() {
 		if (state == LoanState.CURRENT &&
 			Calendar.getInstance().getDate().after(dueDate)) {
 			this.state = LoanState.OVER_DUE;
 		}
-		
 	}
-
 	public boolean isOverDue() {
 		return state == LoanState.OVER_DUE;
 	}
-
 	public Long getId() {
 		return loanId;
 	}
-
-
 	public Date getDueDate() {
 		return dueDate;
 	}
-
 	public String toString() {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
 		StringBuilder sb = new StringBuilder();
 		sb.append("Loan:  ").append(loanId).append("\n")
 		  .append("  Borrower ").append(patron.getId()).append(" : ")
 		  .append(patron.getFirstName()).append(" ").append(patron.getLastName()).append("\n")
 		  .append("  Item ").append(item.getId()).append(" : " )
-		  .append(item.getType()).append("\n")
+		  .append(item.getItemType()).append("\n")
 		  .append(item.getTitle()).append("\n")
 		  .append("  DueDate: ").append(sdf.format(dueDate)).append("\n")
 		  .append("  State: ").append(state);
 		return sb.toString();
 	}
-
-
 	public Patron getPatron() {
 		return patron;
 	}
-
-
 	public Item getItem() {
 		return item;
 	}
-
-
 	public void discharge() {
 		state = LoanState.DISCHARGED;
 	}
-
 }
