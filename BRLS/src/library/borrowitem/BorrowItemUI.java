e630a7563713758e97d320233d58d4510b444634
RishCSU <rsing107@postoffice.csu.edu.au>
2022-08-11 17:56:34 +1000

    Replaced tabs with 4 spaces in BorrowItemUI.java to comply with Rule 9 of the ITC205 Code Style Guidelines

--- BRLS/src/library/borrowitem/BorrowItemUI.java
+++ BRLS/src/library/borrowitem/BorrowItemUI.java
@@ -3,136 +3,136 @@ import java.util.Scanner;
 
 
 public class BorrowItemUI {
-	
-	public static enum BorrowItemUIState { INITIALISED, READY, RESTRICTED, SCANNING, IDENTIFIED, FINALISING, COMPLETED, CANCELLED };
+    
+    public static enum BorrowItemUIState { INITIALISED, READY, RESTRICTED, SCANNING, IDENTIFIED, FINALISING, COMPLETED, CANCELLED };
 
-	private BorrowItemUIState uiState;
-	private BorrowItemControl control;
-	private Scanner scanner;
+    private BorrowItemUIState uiState;
+    private BorrowItemControl control;
+    private Scanner scanner;
 
-	
-	public BorrowItemUI(BorrowItemControl control) {
-		this.control = control;
-		scanner = new Scanner(System.in);
-		uiState = BorrowItemUIState.INITIALISED;
-		control.setUI(this);
-	}
+    
+    public BorrowItemUI(BorrowItemControl control) {
+        this.control = control;
+        scanner = new Scanner(System.in);
+        uiState = BorrowItemUIState.INITIALISED;
+        control.setUI(this);
+    }
 
-	
-	private String getInput(String prompt) {
-		System.out.print(prompt);
-		return scanner.nextLine();
-	}	
-		
-		
-	private void displayOutput(Object object) {
-		System.out.println(object);
-	}
-	
-				
-	public void run() {
-		displayOutput("Borrow Item Use Case UI\n");
-		
-		while (true) {
-			
-			switch (uiState) {			
-			
-			case CANCELLED:
-				displayOutput("Borrowing Cancelled");
-				return;
+    
+    private String getInput(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine();
+    }    
+        
+        
+    private void displayOutput(Object object) {
+        System.out.println(object);
+    }
+    
+                
+    public void run() {
+        displayOutput("Borrow Item Use Case UI\n");
+        
+        while (true) {
+            
+            switch (uiState) {            
+            
+            case CANCELLED:
+                displayOutput("Borrowing Cancelled");
+                return;
 
-				
-			case READY:
-				String patronString = getInput("Swipe patron card (press <enter> to cancel): ");
-				if (patronString.length() == 0) {
-					control.cancel();
-					break;
-				}
+                
+            case READY:
+                String patronString = getInput("Swipe patron card (press <enter> to cancel): ");
+                if (patronString.length() == 0) {
+                    control.cancel();
+                    break;
+                }
 
-				try {
-					long patronId = Long.valueOf(patronString).longValue();
-					control.cardSwiped(patronId);
-				} catch (NumberFormatException e) {
-					displayOutput("Invalid Patron Id");
-				}
-				break;
+                try {
+                    long patronId = Long.valueOf(patronString).longValue();
+                    control.cardSwiped(patronId);
+                } catch (NumberFormatException e) {
+                    displayOutput("Invalid Patron Id");
+                }
+                break;
 
-				
-			case RESTRICTED:
-				getInput("Press <any key> to cancel");
-				control.cancel();
-				break;
-			
-				
-			case SCANNING:
-				String itemStringInput = getInput("Scan Item (<enter> completes): ");
-				if (itemStringInput.length() == 0) {
-					control.borrowingCompleted();
-					break;
-				}
+                
+            case RESTRICTED:
+                getInput("Press <any key> to cancel");
+                control.cancel();
+                break;
+            
+                
+            case SCANNING:
+                String itemStringInput = getInput("Scan Item (<enter> completes): ");
+                if (itemStringInput.length() == 0) {
+                    control.borrowingCompleted();
+                    break;
+                }
 
-				try {
-					int itemId = Integer.valueOf(itemStringInput).intValue();
-					control.itemScanned(itemId);
-				} catch (NumberFormatException e) {
-					displayOutput("Invalid Item Id");
-				} 
-				break;
-					
-				
-			case FINALISING:
-				String answer = getInput("Commit loans? (Y/N): ");
-				if (answer.toUpperCase().equals("N")) {
-					control.cancel();
-				} else {
-					control.commitLoans();
-					getInput("Press <any key> to complete ");
-				}
-				break;
-				
-				
-			case COMPLETED:
-				displayOutput("Borrowing Completed");
-				return;
-	
-				
-			default:
-				displayOutput("Unhandled state");
-				throw new RuntimeException("BorrowItemUI : unhandled state :" + uiState);			
-			}
-		}		
-	}
-
-
-	public void display(Object object) {
-		displayOutput(object);		
-	}
+                try {
+                    int itemId = Integer.valueOf(itemStringInput).intValue();
+                    control.itemScanned(itemId);
+                } catch (NumberFormatException e) {
+                    displayOutput("Invalid Item Id");
+                } 
+                break;
+                    
+                
+            case FINALISING:
+                String answer = getInput("Commit loans? (Y/N): ");
+                if (answer.toUpperCase().equals("N")) {
+                    control.cancel();
+                } else {
+                    control.commitLoans();
+                    getInput("Press <any key> to complete ");
+                }
+                break;
+                
+                
+            case COMPLETED:
+                displayOutput("Borrowing Completed");
+                return;
+    
+                
+            default:
+                displayOutput("Unhandled state");
+                throw new RuntimeException("BorrowItemUI : unhandled state :" + uiState);            
+            }
+        }        
+    }
 
 
-	public void setReady() {
-		uiState = BorrowItemUIState.READY;
-	}
+    public void display(Object object) {
+        displayOutput(object);        
+    }
 
 
-	public void setScanning() {
-		uiState = BorrowItemUIState.SCANNING;
-	}
+    public void setReady() {
+        uiState = BorrowItemUIState.READY;
+    }
 
 
-	public void setRestricted() {
-		uiState = BorrowItemUIState.RESTRICTED;
-	}
+    public void setScanning() {
+        uiState = BorrowItemUIState.SCANNING;
+    }
 
-	public void setFinalising() {
-		uiState = BorrowItemUIState.FINALISING;
-	}
 
-	public void setCompleted() {
-		uiState = BorrowItemUIState.COMPLETED;
-	}
+    public void setRestricted() {
+        uiState = BorrowItemUIState.RESTRICTED;
+    }
 
-	public void setCancelled() {
-		uiState = BorrowItemUIState.CANCELLED;
-	}
+    public void setFinalising() {
+        uiState = BorrowItemUIState.FINALISING;
+    }
+
+    public void setCompleted() {
+        uiState = BorrowItemUIState.COMPLETED;
+    }
+
+    public void setCancelled() {
+        uiState = BorrowItemUIState.CANCELLED;
+    }
 
 }
