99646683a8cf24ccc22d19b4e4060a402296cf77
RishCSU <rsing107@postoffice.csu.edu.au>
2022-08-14 11:47:40 +1000

    Resolved PR4 D4

--- BRLS/src/library/borrowitem/BorrowItemUI.java
+++ BRLS/src/library/borrowitem/BorrowItemUI.java
@@ -37,68 +37,68 @@ public class BorrowItemUI {
             
             switch (uiState) {            
             
-            case CANCELLED:
-                displayOutput("Borrowing Cancelled");
-                return;
+                case CANCELLED:
+                    displayOutput("Borrowing Cancelled");
+                    return;
 
-                
-            case READY:
-                String patronString = getInput("Swipe patron card (press <enter> to cancel): ");
-                if (patronString.length() == 0) {
-                    control.cancel();
-                    break;
-                }
-
-                try {
-                    long patronId = Long.valueOf(patronString).longValue();
-                    control.cardSwiped(patronId);
-                } catch (NumberFormatException e) {
-                    displayOutput("Invalid Patron Id");
-                }
-                break;
-
-                
-            case RESTRICTED:
-                getInput("Press <any key> to cancel");
-                control.cancel();
-                break;
-            
-                
-            case SCANNING:
-                String itemStringInput = getInput("Scan Item (<enter> completes): ");
-                if (itemStringInput.length() == 0) {
-                    control.borrowingCompleted();
-                    break;
-                }
-
-                try {
-                    int itemId = Integer.valueOf(itemStringInput).intValue();
-                    control.itemScanned(itemId);
-                } catch (NumberFormatException e) {
-                    displayOutput("Invalid Item Id");
-                } 
-                break;
                     
-                
-            case FINALISING:
-                String answer = getInput("Commit loans? (Y/N): ");
-                if (answer.toUpperCase().equals("N")) {
+                case READY:
+                    String patronString = getInput("Swipe patron card (press <enter> to cancel): ");
+                    if (patronString.length() == 0) {
+                        control.cancel();
+                        break;
+                    }
+
+                    try {
+                        long patronId = Long.valueOf(patronString).longValue();
+                        control.cardSwiped(patronId);
+                    } catch (NumberFormatException e) {
+                        displayOutput("Invalid Patron Id");
+                    }
+                    break;
+
+                    
+                case RESTRICTED:
+                    getInput("Press <any key> to cancel");
                     control.cancel();
-                } else {
-                    control.commitLoans();
-                    getInput("Press <any key> to complete ");
-                }
-                break;
+                    break;
                 
-                
-            case COMPLETED:
-                displayOutput("Borrowing Completed");
-                return;
-    
-                
-            default:
-                displayOutput("Unhandled state");
-                throw new RuntimeException("BorrowItemUI : unhandled state :" + uiState);            
+                    
+                case SCANNING:
+                    String itemStringInput = getInput("Scan Item (<enter> completes): ");
+                    if (itemStringInput.length() == 0) {
+                        control.borrowingCompleted();
+                        break;
+                    }
+
+                    try {
+                        int itemId = Integer.valueOf(itemStringInput).intValue();
+                        control.itemScanned(itemId);
+                    } catch (NumberFormatException e) {
+                        displayOutput("Invalid Item Id");
+                    } 
+                    break;
+                        
+                    
+                case FINALISING:
+                    String answer = getInput("Commit loans? (Y/N): ");
+                    if (answer.toUpperCase().equals("N")) {
+                        control.cancel();
+                    } else {
+                        control.commitLoans();
+                        getInput("Press <any key> to complete ");
+                    }
+                    break;
+                    
+                    
+                case COMPLETED:
+                    displayOutput("Borrowing Completed");
+                    return;
+        
+                    
+                default:
+                    displayOutput("Unhandled state");
+                    throw new RuntimeException("BorrowItemUI : unhandled state :" + uiState);            
             }
         }        
     }
